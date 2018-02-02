77557fe0ef501d337fcd43f327b5012735c27f3c
Lucas Dohmen <lucas@dohmen.io>
2018-02-02 23:05:52 +0100

    Evaluate Nodes

--- lib/halunke/nodes.rb
+++ lib/halunke/nodes.rb
@@ -9,20 +9,48 @@ module Halunke
     def concat(other)
       Nodes.new(nodes.concat(other.nodes))
     end
+
+    def eval(context)
+      val = nil
+
+      nodes.each do |node|
+        val = node.eval(context)
+      end
+
+      val
+    end
   end
 
   LiteralNode = Struct.new(:value)
 
   class NumberNode < LiteralNode
+    def eval(context)
+      context[:Number].create_instance(value)
+    end
   end
 
   class StringNode < LiteralNode
+    def eval(context)
+      context[:String].create_instance(value)
+    end
   end
 
   class BarewordNode < LiteralNode
   end
 
-  MessageSendNode = Struct.new(:receiver, :message)
+  MessageSendNode = Struct.new(:receiver, :message) do
+    def eval(context)
+      receiver.eval(context).receive_message(*message.eval(context))
+    end
+  end
 
-  MessageNode = Struct.new(:nodes)
+  MessageNode = Struct.new(:nodes) do
+    def eval(context)
+      case nodes.length
+      when 1 then [nodes[0].value, []]
+      when 2 then [nodes[0].value, [nodes[1].eval(context)]]
+      else raise "Not implemented"
+      end
+    end
+  end
 end
