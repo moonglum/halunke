b5eba7eb442a44e1b259395487d64e782ec69c4c
Dirk Breuer <mail@breuer.io>
2019-03-04 21:41:28 +0100

    Ignore "warning: mismatched indentations" for generated files

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,9 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "halunke"
 
+# NB: Since both parser.rb and tokenizer.rb are generated and contain some mismatched indentations we want to ignore those warnings.
+require "warning"
+Warning.ignore(/warning: mismatched indentations/, File.expand_path("../../lib/halunke/parser.rb", __FILE__))
+Warning.ignore(/warning: mismatched indentations/, File.expand_path("../../lib/halunke/tokenizer.rb", __FILE__))
+
 require "minitest/autorun"
